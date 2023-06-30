2b91d301a77ac4235b30ff1adc1acb6520a294d4
AlessandraMariana <ale.souzza12@gmail.com>
2023-06-29 23:33:08 -0300

    update

--- src/components/temas/deletarTema/DeletarTema.tsx
+++ src/components/temas/deletarTema/DeletarTema.tsx
@@ -3,14 +3,74 @@ import { Card, CardActions, CardContent, Button, Typography} from '@material-ui/
 import {Box} from '@mui/material';
 import './DeletarTema.css';
 import { useNavigate, useParams } from 'react-router-dom';
-import useLocalStorage from 'react-use-localstorage';
 import { buscaId, deleteId } from '../../../services/Service';
 import Tema from '../../../models/Tema';
-
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { toast } from 'react-toastify';
 
 function DeletarTema() {
-  
-          
+  let navigate = useNavigate();
+    const { id } = useParams<{id: string}>();
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+      (state) => state.tokens
+    );
+    const [tema, setTema] = useState<Tema>()
+
+    useEffect(() => {
+        if (token == "") {
+          toast.error('Você precisa estar logado', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: "colored",
+            progress: undefined,
+            });
+            navigate("/login")
+    
+        }
+    }, [token])
+
+    useEffect(() =>{
+        if(id !== undefined){
+            findById(id)
+        }
+    }, [id])
+
+    async function findById(id: string) {
+        buscaId(`/tema/${id}`, setTema, {
+            headers: {
+              'Authorization': token
+            }
+          })
+        }
+
+        function sim() {
+          navigate('/temas')
+            deleteId(`/tema/${id}`, {
+              headers: {
+                'Authorization': token
+              }
+            });
+            toast.success('Tema deletado com sucesso', {
+              position: "top-right",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: false,
+              draggable: false,
+              theme: "colored",
+              progress: undefined,
+              });
+          }
+        
+          function nao() {
+            navigate('/temas')
+          }
+      
   return (
     <>
       <Box m={2}>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router';
 import { useSelector } from 'react-redux';
 import { TokenState } from '../../store/tokens/tokensReducer';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import {Box} from '@mui/material';
 
 
@@ -19,7 +20,16 @@ function Home() {
     
     useEffect(() => {
       if (token == "") {
-          alert("Você precisa estar logado")
+        toast.error('Você precisa estar logado', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: "colored",
+            progress: undefined,
+        });
           navigate("/login")
   
       }
