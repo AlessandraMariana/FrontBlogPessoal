c9b1551e842aef070d55e1f624bca1e60a586e71
AlessandraMariana <ale.souzza12@gmail.com>
2023-07-05 00:34:03 -0300

    atualiza

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import React,  { useEffect } from 'react';
 import {Typography, Grid, Button} from '@material-ui/core';
 import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
-import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
+import ModalPostagem from '../../components/postagens/modalpostagem/ModalPostagem';
 import './Home.css';
 import { useNavigate } from 'react-router';
 import { useSelector } from 'react-redux';
